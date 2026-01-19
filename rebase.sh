03a40fd15fd8f5443c7fbb7866b6f8e8f0228962
Charro Gruver <cgruver@redhat.com>
2026-01-19 07:57:28 -0500

    update to v19.2.3

---
+++ rebase.sh
@@ -0,0 +1,6 @@
+
+mkdir -p rebase
+
+curl -o ./rebase/operator-openshift.yaml https://raw.githubusercontent.com/rook/rook/refs/heads/release-1.18/deploy/examples/operator-openshift.yaml
+curl -o ./rebase/common.yaml https://raw.githubusercontent.com/rook/rook/refs/heads/release-1.18/deploy/examples/common.yaml
+curl -o ./rebase/crds.yaml https://raw.githubusercontent.com/rook/rook/refs/heads/release-1.18/deploy/examples/crds.yaml
